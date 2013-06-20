ff0f58d8e6a6ef7c71fb9600a4c6ea837dfe9cf2
Naren Chelluri <csnaren@gmail.com>
2013-06-20 21:39:58 +0530

    §

--- www/js/index.js
+++ www/js/index.js
@@ -36,10 +36,6 @@ var app = {
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
         var ref = window.open('http://pedesorange.rapapp.com/m', '_self', 'location=no');
-        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-        ref.addEventListener('exit', function(event) { alert(event.type); });
     },
     //onLine event
     onOnline:function(){
