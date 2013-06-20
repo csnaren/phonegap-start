924e2cae5d1259e794c611b23fc0f8fab79662a0
Naren Chelluri <csnaren@gmail.com>
2013-06-20 21:22:32 +0530

    Redirect to rapapp application

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+        var ref = window.open('http://pedesorange.rapapp.com/m', '_self', 'location=no');
+        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+        ref.addEventListener('exit', function(event) { alert(event.type); });
     },
     //onLine event
     onOnline:function(){
